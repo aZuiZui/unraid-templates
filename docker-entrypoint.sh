aabdbc68014a45f285b57b056daca65e86cfae70
aZuiZui <110262485+aZuiZui@users.noreply.github.com>
2025-08-10 15:33:27 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # Fallback: if script doesn't exist in mounted volume, download it
 if [ ! -f /usr/local/data/fan_control.sh ]; then
@@ -8,17 +8,3 @@ if [ ! -f /usr/local/data/fan_control.sh ]; then
     curl -sSL https://raw.githubusercontent.com/aZuiZui/unraid-templates/refs/heads/main/fan_control.sh -o /usr/local/data/fan_control.sh
     chmod +x /usr/local/data/fan_control.sh
 fi
-
-# Ensure the log file exists
-touch /usr/local/data/fan_control.log
-chmod 666 /usr/local/data/fan_control.log
-
-# Create cron job
-echo "SHELL=/bin/bash" > /etc/cron.d/fan_control
-echo "PATH=$PATH" >> /etc/cron.d/fan_control
-echo "* * * * * root /usr/local/data/fan_control.sh >> /usr/local/data/fan_control.log 2>&1" >> /etc/cron.d/fan_control
-chmod 0644 /etc/cron.d/fan_control
-
-# Export environment for cron
-# env > /usr/local/bin/env_file
-
